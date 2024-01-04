fbfcc380de50944acf63b6369136fa01046659f3
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-01-04 15:44:53 +0100

    bridging classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+/* eslint-disable no-underscore-dangle */
+  constructor(amount, currency) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('Currency must be a currency');
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be a currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+}
