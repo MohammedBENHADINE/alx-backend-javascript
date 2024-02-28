a80f271a2097dbeabad662c992f7aa6d84cffe48
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-02-28 23:43:00 +0100

    renew

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
+process.stdin.once('data', (chunk) => {
+  const name = chunk.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
 process.on('exit', () => {
   console.log('This important software is now closing');
-});
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is ${chunk}`);
-  }
 });
