9106175a16068a990271e297a65407239d358e6c
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-02-28 23:46:48 +0100

    new version

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,14 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.once('data', (chunk) => {
-  const name = chunk.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
-process.on('exit', () => {
-  console.log('This important software is now closing');
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
