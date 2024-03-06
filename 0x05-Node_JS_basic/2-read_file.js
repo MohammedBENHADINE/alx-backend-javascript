f51d625ea3f87f6d292ec1bd8ae95769c053569b
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-03-06 18:13:52 +0100

    fixing issue

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,11 +4,6 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.trim().split('\n');
-
-    if (lines.length === 0) {
-      throw new Error('Cannot load the database');
-    }
-
     const students = [];
     const fields = {};
 
